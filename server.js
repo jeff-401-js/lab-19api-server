d6fc4d713d4801ebab816fe05ec98f3b2fe7a80b
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-20 19:51:35 -0700

    server created

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+
+
+module.exports = {
+  server: app,
+  start: port => {
+    let PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`WE HERE ALL ON PORT ${PORT}`));
+  },
+};
