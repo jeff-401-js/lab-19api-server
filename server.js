aa098a14d5d6042be2f821c47f1eba44a8c43898
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-20 19:55:12 -0700

    read

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const app = express();
 
 app.use(express.json());
 
+const Q = require('@nmq/q/client');
 
+app.get('/read', (res) => {
+  let payload = {
+    name: 'read',
+    data: `READ: event just happened!`,
+  };
+  Q.publish('files', 'read', JSON.stringify(payload));
+  res.send('get');
+});
 
 
 module.exports = {
